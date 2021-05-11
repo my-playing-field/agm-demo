30221546be8225b28029c44a4caf94d679e73be1
uvin6667 <uvin6667@gmail.com>
2021-05-11 16:03:53 +0530

    Import AgmCoreModule and set api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC7OpIMGoiT3fv7Hp8GdU95w9mF6ZlvJmc'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
